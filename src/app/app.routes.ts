e52ba061d779c58d4a8c0565d17bb594f67f47fa
Admin <Admin@DESKTOP-S5DQKIR>
2026-01-28 19:52:35 +0100

    Implementing and Configuring the app.routes for the WebApp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { RouterModule ,Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { AddApplicationComponent } from './components/add-application/add-application.component';
+import { ListApplicationsComponent } from './components/list-applications/list-applications.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'applications', pathMatch: 'full' },
+    { path: 'applications', component: ListApplicationsComponent },
+    { path: 'add', component: AddApplicationComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutes { }
